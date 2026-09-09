fc08bdbdc0161d68354ecdc537cc59c33adf137e
Pedroguibas <pedroguibas123@gmail.com>
2026-09-09 19:18:17 -0300

    feat: :sparkles: Implements user patch routes
    Implements basic user patch routes as well as password patch and restore routes. Restore uses a code valid for 10 minutes sent to user's that allows the them to change their password

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule, ObserveInstrument } from './app.module.js';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     instrument: ObserveInstrument,
   });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   app.enableCors({
     origin: true,
     credentials: true,
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller.js';
 import { UsersService } from './users.service.js';
 import { SupabaseModule } from '../supabase/supabase.module.js';
+import { ConfigModule } from '@nestjs/config';
+import { RedisModule } from '../redis/redis.module.js';
 
 @Module({
-  imports: [SupabaseModule],
+  imports: [SupabaseModule, RedisModule, ConfigModule],
   controllers: [UsersController],
   providers: [UsersService],
 })
